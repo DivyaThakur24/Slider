c1f39075b526f24e305cd169e72f6a622bf2a731
DivyaThakur24 <greyblack2406@gmail.com>
2018-03-22 16:14:10 +0530

    Files

---
+++ app/src/main/java/com/example/android/landingpageearsleep/SliderAdapter.java
@@ -0,0 +1,90 @@
+package com.example.android.landingpageearsleep;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Asus-Mukul on 21-03-2018.
+ */
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    //Arrays
+    public int[] slide_images = {
+
+            R.drawable.eat_icon,
+            R.drawable.sleep_icon,
+            R.drawable.code_icon,
+            R.drawable.repeat_icon,
+            R.drawable.download_icon
+    };
+
+    public String[] slide_headings = {
+
+            "EAT",
+            "SLEEP",
+            "CODE",
+            "REPEAT",
+            "DOWNLOAD NOW !!!"
+
+    };
+
+    public String[] slide_descs = {
+
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            ""
+
+    };
+
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == (LinearLayout) object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImageView = (ImageView) view.findViewById(R.id.slide_image);
+        TextView slideHeading = (TextView) view.findViewById(R.id.slide_heading);
+        TextView slideDescription = (TextView) view.findViewById(R.id.slide_desc);
+
+        slideImageView.setImageResource(slide_images[position]);
+        slideHeading.setText(slide_headings[position]);
+        slideDescription.setText(slide_descs[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout)object);
+    }
+
+}
